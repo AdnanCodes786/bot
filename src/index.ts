f14d1d8ac57a10cd7058be0b0aafcad242c45de9
Adnan Akhtar <adnan.akhtar_transfi@Adnans-MacBook-Air.local>
2026-02-01 23:54:43 +0530

    entry

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { connectDB } from "./db/connect";
 import { registerWebhook } from "./utils/webhook-setup";
 dotenv.config();
 
+
 async function start(): Promise<void> {
   await registerWebhook("https://keen-sparrow-79.webhook.cool");
   await connectDB();
