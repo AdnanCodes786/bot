4c49f0eb00dcd1651aab92e039779440b8e10c6f
Adnan <adnanbarwatiya32@gmail.com>
2026-02-07 15:39:54 +0530

    checks for added message-added

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ dotenv.config();
 
 import express from "express";
 import { connectDB } from "./db/connect";
+import { GroupModel } from "./models/group";
+import { MessageModel } from "./models/mesage";
 connectDB();
 const app = express();
 const PORT = 3000;
@@ -16,9 +18,48 @@ app.get("/", (req, res) => {
   });
 });
 
-app.post("/message", (req, res) => {
-  console.log("📩 Telegram update received");
-  console.log(JSON.stringify(req.body, null, 2));
+app.post("/message", async (req, res) => {
+  const update = req.body;
+  if (update.message) {
+    return res.status(400).json({
+      success: false,
+      message: "no message content found",
+    });
+  }
+  const message = update.message;
+  const { chat, from, text } = message;
+
+  if (!chat || (chat.type !== "group" && chat.type !== "supergroup")) {
+    return res.sendStatus(200);
+  }
+
+  if (!from.is_bot) {
+    return res.sendStatus(200);
+  }
+  if (text.startsWith("/")) {
+    return res.sendStatus(200);
+  }
+
+  const telegramGroupId=chat.id;
+
+  await GroupModel.findOneAndUpdate(
+      { telegramGroupId },
+      {
+        telegramGroupId,
+        title: chat.title,
+        plan: "free",
+        summaryRunsUsed: 0,
+        summaryRunsLimit: 5,
+      },
+      { upsert: true, new: true }
+    );
+
+    await MessageModel.create({
+      telegramGroupId,
+      userId: from.id,
+      username: from.username || from.first_name || "unknown",
+      text: text.trim(),
+    });
 
   res.status(200).json({
     success: true,
@@ -28,8 +69,6 @@ app.post("/message", (req, res) => {
 });
 
 async function start() {
-   
-
   app.listen(PORT, () => {
     console.log("Backend is running now");
   });
